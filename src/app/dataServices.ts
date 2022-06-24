667d1e917a2a86db3905a159c6fc68959c376e6a
usuario <laureano.bentancor@gmail.com>
2022-06-24 15:22:41 -0300

    Guardado y recuperacion de datos desde bd Modificar registros incompleta

--- src/app/dataServices.ts
+++ src/app/dataServices.ts
@@ -8,13 +8,13 @@ export class DataServices{
 
     // tslint:disable-next-line: typedef
     cargarPersonas(){
-      return this.httpClient.get('https://listado-personas-74cde-default-rtdb.firebaseio.com/datos.json');
+      return this.httpClient.get('https://listadopersonas-4e9fa-default-rtdb.firebaseio.com/datos.json');
     }
 
     // tslint:disable-next-line: typedef
     guardarPersonas(personas: Persona[]){
 
-    this.httpClient.put('https://listado-personas-74cde-default-rtdb.firebaseio.com/datos.json', personas).subscribe(
+    this.httpClient.put('https://listadopersonas-4e9fa-default-rtdb.firebaseio.com/datos.json', personas).subscribe(
       response =>  console.log( ' resultado de Guardar Personas' + response),
       error => console.log('error al guardar Persona' + error)
     );
@@ -22,19 +22,19 @@ export class DataServices{
     }
 
     // tslint:disable-next-line: typedef
-    modificarPersona(index: number, personas: Persona []){
+    modificarPersona(index: number, personas: Persona[]){
 
       let url: string;
-      url = ' https://listado-personas-74cde-default-rtdb.firebaseio.com/datos/' + index + '.json';
+      url = ' https://listadopersonas-4e9fa-default-rtdb.firebaseio.com/datos' + index + '.json';
 
-      this.httpClient.put (url, personas).subscribe(respuesta => console.log('respuesta de Modificar Persona' + respuesta),
+      this.httpClient.put (url, personas).subscribe(response => console.log('respuesta de Modificar Persona' + response),
                                                     error => console.log('error al modificar Persona' + error));
     }
 
     // tslint:disable-next-line: typedef
     eliminarPersona(index: number){
       let url: string;
-      url = ' https://listado-personas-74cde-default-rtdb.firebaseio.com/datos/' + index + '.json';
+      url = ' https://listadopersonas-4e9fa-default-rtdb.firebaseio.com/datos' + index + '.json';
 
       this.httpClient.delete (url).subscribe(respuesta => console.log('respuesta de borrar Persona' + respuesta),
                                              error => console.log('error al borrar Persona' + error));
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -12,22 +12,20 @@ import { PersonasService } from '../persona.service';
 export class PersonasComponent implements OnInit {
 
   constructor(private personasService: PersonasService, private router: Router){}
-
-
-  personas: Persona[];
+  personas: Persona[] = [];
 
   // tslint:disable-next-line: typedef
   ngOnInit(){
      // tslint:disable-next-line: one-variable-per-declaration
 
     this.personasService.obtenerPersonas().subscribe(
-    (personas: Persona[]) => {
-    this.personas = personas;
-    this.personasService.setPersonas(personas);
 
-
+    /* observable no admite objetos de tipo Persona para que no de error se coloco en los paramatros de la funcion flecha de tipo Object */
+    // tslint:disable-next-line: ban-types
+    (personas: Object) => {
+    this.personas = personas as Persona[];
+    this.personasService.setPersonas(personas as Persona[]);
     }
-
     );
 
   }
@@ -37,4 +35,5 @@ export class PersonasComponent implements OnInit {
     this.router.navigate(['personas/agregar']);
   }
 }
+
 
