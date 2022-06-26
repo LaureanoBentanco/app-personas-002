ce7ccd4c54e4107c23c2b561a4fd3e68f1b98e24
usuario <laureano.bentancor@gmail.com>
2022-06-25 22:54:32 -0300

    Reparacion de error al istalar paquete de firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
 
   titulo = 'Listado de Personas';
 
+ngOnInit(): void {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyBfEezSObsJ-b4pPFs46KaIPMp_sWm1X2g',
+    authDomain: 'listadopersonas-4e9fa.firebaseapp.com'
+  });
+}
 }
